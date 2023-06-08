e5dc480d6ab91f3a7176b5c04504f7bb206c0b22
Raikarus <ivatim32@yandex.ru>
2023-06-08 22:03:56 +0300

    1

--- xlsx_parcer/Program.cs
+++ xlsx_parcer/Program.cs
@@ -33,28 +33,48 @@ namespace xlsx_parcer
 
             string path = @"21.xml";
 
-            
+            int last_i = 0;
             try
             {
-                for(int i = 1; i < Find_name(path).Length;i++)
-                {
-                    fields.Add(new field(Find_name(path)[i], Find_INN(path)[i], Find_type_nalog(path)[i], Find_nalog(path)[i]));
-                }
+                // Создайте экземпляр объекта Workbook, который представляет файл Excel.
+                Workbook wb = new Workbook();
 
-                foreach (field field in fields)
-                {
-                    Console.WriteLine(field.name + " " +field.INN +" "+field.type_nalog +" " + field.nalog);
-                }
+                // Когда вы создаете новую книгу, в книгу добавляется по умолчанию «Лист1».
+                Worksheet sheet = wb.Worksheets[0];
 
-                WriteExcel();
+                for (int i = 1; i < Find_name(path).Length;i++)
+                {
+                    Cell cell = sheet.Cells[i+last_i, 0];
+                    cell.PutValue(Find_INN(path)[i]);
+
+                    cell = sheet.Cells[i+last_i, 2];
+                    cell.PutValue(Find_name(path)[i]);
+
+                    if (Find_type_nalog(path)[i]=="УСНО") 
+                    {
+                        cell = sheet.Cells[i+last_i, 3];
+                        cell.PutValue(Find_type_nalog(path)[i]);
+                        cell = sheet.Cells[i+last_i, 4];
+                        cell.PutValue(Find_type_nalog(path)[i]);
+                    }
+                    else
+                    {
+                        cell = sheet.Cells[i+last_i, 4];
+                        cell.PutValue(Find_type_nalog(path)[i]);
+                    }
+                    cell = sheet.Cells[i+last_i, 5];
+                    cell.PutValue(Find_nalog(path)[i]);
+                }
+                last_i+=Find_name(path).Length;
+                // Сохраните Excel как файл .xlsx.
+                wb.Save("Excel.xlsx", SaveFormat.Xlsx);
+
             }
             catch (Exception e)
             {
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
-
-           // Console.WriteLine("Hello World!");
         }
 
         public static int Count_Str(string str, char ch)
@@ -169,15 +189,10 @@ namespace xlsx_parcer
                     {
                         try
                         {
-
-                            words[i] = words[i].Substring(0, words[i].IndexOf("\"/>") + 1);
-
-
-                         //   Console.WriteLine(words[i]);
+                            words[i] = words[i].Substring(1, words[i].IndexOf("\"/>")-1);
                         }
                         catch
                         {
-                          //  Console.WriteLine("каво");
                         }
 
                     }
@@ -213,11 +228,11 @@ namespace xlsx_parcer
 
                             if(words[i] == "Налог, взимаемый в связи с  применением упрощенной  системы налогообложения")
                             {
-                                words[i] = "\"УСНО\"";
+                                words[i] = "УСНО";
                             }
                             else
                             {
-                                words[i] = "\"\"";
+                                words[i] = "";
                             }
                           //  Console.WriteLine(words[i]);
                         }
@@ -247,7 +262,7 @@ namespace xlsx_parcer
             Worksheet sheet = wb.Worksheets[0];
 
             // Получите доступ к ячейке «A1» на листе.
-            Cell cell = sheet.Cells["A1"];
+            Cell cell = sheet.Cells[0,1];
 
             // Введите «Привет, мир!» текст в ячейку «А1».
             cell.PutValue("Hello World!");
